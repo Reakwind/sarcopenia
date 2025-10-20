9af41efe1882ea661dfd59f025b3a486a1f434fc
Etay Cohen <etaycohen@Etays-MacBook-Air.local>
2025-10-20 19:39:36 +0300

    make the app self contained

--- shiny-dashboard/sarcDash/app.R
+++ shiny-dashboard/sarcDash/app.R
@@ -1,6 +1,5 @@
 # Launch the ShinyApp (Do not remove this comment)
 # This is the entry point for Posit Connect Cloud deployment
-
 pkgload::load_all(export_all = FALSE, helpers = FALSE, attach_testthat = FALSE)
 options("golem.app.prod" = TRUE)
 sarcDash::run_app()
